520930eb43ed8c83c59f9c884c7e8a3a880258bd
jenous11 <jenousdongol11@gmail.com>
2025-10-06 23:00:52 +0545

    added some bootstrap

--- frontpage.php
+++ frontpage.php
@@ -4,22 +4,49 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Frontpage</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
 </head>
 <body>
-    <form action="includes/insert.php" method="post">
-        username:
-        <input type="text"  name="username" placeholder="enter your name">
-        <br>
-        <br>
-        email:
-        <input type="email"  name="email" placeholder="enter your email">
-        <br>
-        <br>
-        password:
-        <input type="password" name="password"placeholder="enter your password">
-        <br>
-        <br>
-        <input type="submit">
-    </form>
+    <!-- <form action="includes/insert.php" method="post">
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Email address</label>
+            username:
+            <input type="text"  name="username" placeholder="enter your name">
+            <br>
+            <br>
+            email:
+            <input type="email"  name="email" placeholder="enter your email">
+            <br>
+            <br>
+            password:
+            <input type="password" name="password"placeholder="enter your password">
+            <br>
+            <br>
+            <input type="submit">
+        </div>
+    </form> -->
+
+    <!-- form action  -->
+
+    <form>
+  <div class="mb-3">
+    <input type="username" class="form-control" id=" usernameInput ">
+  </div>
+  <div class="mb-3">
+    <input type="password" class="form-control" id="exampleInputPassword1">
+  </div>
+  <div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+
+
+
+
+
 </body>
 </html>
--- userpage.php
+++ userpage.php
@@ -1,12 +1,10 @@
 <?php
     session_start();
     if(isset($_SESSION['username'])){
-    
         echo "<p>"."Welcome ".$_SESSION['username']." to your personal page!! </p>";
         echo "<a href='logout.php'><button>logout</button></a>";
     }
     else{
         header('Location: frontpage.php ');
     }
-
     ?>
